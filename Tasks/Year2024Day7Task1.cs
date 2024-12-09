fd9591ebd4ed543d315df51f2d7103fc6de34bea
MrakDun-desu <m.danco@protonmail.com>
2024-12-09 23:01:17 +0100

    Year 2024 day 7

---
+++ Tasks/Year2024Day7Task1.cs
@@ -0,0 +1,36 @@
+namespace AdventOfCode.Tasks;
+
+public class Year2024Day7Task1 : IDailyTask
+{
+    private static HashSet<ulong> GetPossibleResults(ulong state, ulong[] next)
+    {
+        if (next.Length == 0)
+        {
+            return [state];
+        }
+
+        var rest = next[1..];
+        var output = GetPossibleResults(state + next[0], rest);
+        output.UnionWith(GetPossibleResults(state * next[0], rest));
+        return output;
+    }
+
+    public string Execute(string input)
+    {
+        var inputLines = input.Split("\n")[..^1];
+        ulong sum = 0;
+        foreach (var line in inputLines)
+        {
+            var splitLine = line.Split(": ");
+
+            var result = ulong.Parse(splitLine[0]);
+            var numbers = splitLine[1].Split(' ').Select(ulong.Parse).ToArray();
+
+            if (GetPossibleResults(numbers[0], numbers[1..]).Contains(result))
+            {
+                sum += result;
+            }
+        }
+        return sum.ToString();
+    }
+}
