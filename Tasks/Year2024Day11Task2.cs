ccd3ae74828b713ab73aa93a5605db660521ed13
MrakDun-desu <m.danco@protonmail.com>
2024-12-11 23:34:02 +0100

    2024 day 11 task 2

--- Tasks/Year2024Day11Task2.cs
+++ Tasks/Year2024Day11Task2.cs
@@ -4,6 +4,7 @@ namespace AdventOfCode.Tasks;
 
 public class Year2024Day11Task2 : IDailyTask {
     private readonly NumberFormatInfo Format = CultureInfo.InvariantCulture.NumberFormat;
+    private readonly Dictionary<(ulong, uint), ulong> Cache = [];
 
     private static ulong NumDigits(ulong num) {
         var numDigits = 1uL;
@@ -14,34 +15,33 @@ public class Year2024Day11Task2 : IDailyTask {
         }
     }
 
-    private static UInt128 CountStones(ulong stone, uint depth = 75) {
+    private ulong CountStones(ulong stone, uint depth = 75) {
         if (depth == 0) {
             return 1;
         }
-        depth--;
-        // while the number doesn't have even digits, no need to recurse
-        var numDigits = NumDigits(stone);
-        while (numDigits % 2 != 0) {
-            if (depth-- == 0) {
-                return 1;
-            }
-            stone = stone == 0 ? 1uL : stone * 2024uL;
-            numDigits = NumDigits(stone);
+        if (Cache.TryGetValue((stone, depth), out var output)) {
+            return output;
         }
-        var upperHalf = stone / MyMath.Pow(10uL, numDigits / 2);
-        var lowerHalf = stone - (upperHalf * MyMath.Pow(10uL, numDigits / 2));
-        return CountStones(upperHalf, depth) + CountStones(lowerHalf, depth);
+        if (stone == 0) {
+            output = CountStones(1, depth - 1);
+        } else if (NumDigits(stone) % 2 == 0) {
+            var numDigits = NumDigits(stone);
+            var upperHalf = stone / MyMath.Pow(10uL, numDigits / 2);
+            var lowerHalf = stone - (upperHalf * MyMath.Pow(10uL, numDigits / 2));
+            output = CountStones(upperHalf, depth - 1) + CountStones(lowerHalf, depth - 1);
+        } else {
+            output = CountStones(stone * 2024, depth - 1);
+        }
+        Cache[(stone, depth)] = output;
+        return output;
     }
 
     public string Execute(string input) {
         input = input.Trim();
         var stones = input.Split(' ').Select(uint.Parse).ToArray();
-        if (stones.Length < 5) {
-            return "";
-        }
-        UInt128 sum = 0;
+        ulong sum = 0;
         for (var i = 0; i < stones.Length; i++) {
-            sum += CountStones(stones[i], 75);
+            sum += CountStones(stones[i]);
         }
 
         return sum.ToString(Format);
